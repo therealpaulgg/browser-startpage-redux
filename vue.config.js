c947e629161dea45be7d92efc76447342af71e04
agellai <agellai@asu.edu>
2020-05-15 14:33:42 -0700

    test??

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
     transpileDependencies: ["vuetify"],
+    pwa: {
+        name: "Startpage Redux",
+        themeColor: "#4DBA87",
+        msTileColor: "#000000",
+        appleMobileWebCapable: "yes",
+        appleMobileWebAppStatusBarStyle: "black",
+    },
 }
